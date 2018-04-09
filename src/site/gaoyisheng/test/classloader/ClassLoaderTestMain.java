b5bf1e9ee90d2c06b86bcec76e0dc51dff95b43b
timo1160139211 <1160139211@qq.com>
2018-04-09 17:30:56 +0800

    测试三类ClassLoader所依赖的系统参数

--- src/site/gaoyisheng/test/classloader/ClassLoaderTestMain.java
+++ src/site/gaoyisheng/test/classloader/ClassLoaderTestMain.java
@@ -3,26 +3,33 @@ package site.gaoyisheng.test.classloader;
 
 public class ClassLoaderTestMain {
 
-    private static String ClassA = "site.gaoyisheng.test.classloader.A";
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
 
         try {
+            String classA = "site.gaoyisheng.test.classloader.A";
+
             //仅载入文件， 不 初始化
             System.out.println("classLoader.loadClass ==> 系统加载class A");
-            Class c = classLoader.loadClass(ClassA);
+            classLoader.loadClass(classA);
 
             //载入文件 并 初始化
             System.out.println("Class.forName ==> 加载 class A");
-            Class.forName(ClassA);
+            Class.forName(classA);
+
+            System.out.println("\n" +
+                    "Bootstrap Loader依赖'sun.boot.class.path'系统参数:" + System.getProperty("sun.boot.class.path"));
+            System.out.println("\n" +
+                    "ExtClassLoader依赖'java.ext.dirs':系统参数" + System.getProperty("java.ext.dirs"));
+            System.out.println("\n" +
+                    "AppClassLoader依赖'java.class.path'系统参数:" + System.getProperty("java.class.path"));
 
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
 
 
-        System.out.println("hah123");
+        System.out.println("haha123  done");
     }
 }
